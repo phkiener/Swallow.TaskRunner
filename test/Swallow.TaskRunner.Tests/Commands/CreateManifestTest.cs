d5ac107303855764f4259bdcf655646b62f4b2c1
Philipp Kiener <kiener.phil@gmail.com>
2026-09-21 16:23:28 +0200

    oh wait i do got tests

---
+++ test/Swallow.TaskRunner.Tests/Commands/CreateManifestTest.cs
@@ -0,0 +1,35 @@
+﻿using Swallow.TaskRunner.Commands;
+using Swallow.TaskRunner.Serialization;
+
+namespace Swallow.TaskRunner.Test.Commands;
+
+public sealed class CreateManifestTest
+{
+    private readonly CreateManifest command = new();
+
+    [Test]
+    public async Task CreatesNewManifest()
+    {
+        using var context = TestCommandContext.Create();
+        await command.RunAsync(context, []);
+
+        var expectedFile = Path.Combine(context.CurrentDirectory, ".config", "dotnet-tasks.json");
+        await Assert.That(context.WrittenOutput).Contains(expectedFile);
+        await Assert.That(context.WrittenOutput).Contains("Created new task manifest");
+
+        await using var fileStream = File.OpenRead(expectedFile);
+        var manifest = ManifestReader.ReadAsync(fileStream, context.CancellationToken);
+
+        Assert.NotNull(manifest);
+    }
+
+    [Test]
+    public async Task RunningTwice_WorksWithoutExceptions()
+    {
+        using var context = TestCommandContext.Create();
+        await command.RunAsync(context, []);
+        await command.RunAsync(context, []);
+
+        await Assert.That(context.WrittenOutput).Contains("Task manifest already exists");
+    }
+}
---
+++ test/Swallow.TaskRunner.Tests/TestCommandContext.cs
@@ -0,0 +1,44 @@
+﻿using Swallow.TaskRunner.Abstractions;
+
+namespace Swallow.TaskRunner.Test;
+
+public sealed class TestCommandContext : ICommandContext, IDisposable
+{
+    private static readonly string TestContextDirectory = Path.Combine(Path.GetTempPath(), "Swallow.TaskRunner", nameof(TestCommandContext));
+    private readonly string initialDirectory;
+
+    private TestCommandContext(string directory)
+    {
+        initialDirectory = directory;
+        CurrentDirectory = initialDirectory;
+    }
+
+    public TextWriter Output { get; } = new StringWriter();
+    public TextWriter Error { get; } = new StringWriter();
+    public string CurrentDirectory { get; }
+    public CancellationToken CancellationToken => CancellationToken.None;
+
+    public Task<int> Execute(string command)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string WrittenOutput => Output.ToString() ?? "";
+    public string WrittenError => Error.ToString() ?? "";
+
+    public void Dispose()
+    {
+        Directory.Delete(initialDirectory, recursive: true);
+
+        Output.Dispose();
+        Error.Dispose();
+    }
+
+    public static TestCommandContext Create()
+    {
+        var contextPath = Path.Combine(TestContextDirectory, Guid.NewGuid().ToString("D"));
+        Directory.CreateDirectory(contextPath);
+
+        return new TestCommandContext(contextPath);
+    }
+}
